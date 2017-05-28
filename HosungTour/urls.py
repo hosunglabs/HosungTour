31245b04c1068dd43a2e42a1bfe66c80be08c0e6
HosungKim <tomatoto20@outlook.com>
2017-05-28 11:04:22 +0900

    create 4th

--- HosungTour/urls.py
+++ HosungTour/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'accounts/', include('accounts.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^hotu/', include ('hotu.urls')),
+    url(r'^shop/', include('shop.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+    url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 import re
+from django.conf import settings
 from django.db import models
 from django.forms import ValidationError
 # Create your models here.
@@ -8,7 +9,12 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
-    author = models.CharField(max_length=20)
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=100, verbose_name='제목',
         help_text='포스팅의 제목을 입력해주세요. 최대 100자 내외.')
     tags = models.CharField(max_length=100, blank=True)
@@ -16,5 +22,27 @@ class Post(models.Model):
     lnglat = models.CharField(max_length=50, blank=True,
         validators=[lnglat_validator],
         help_text='위도/경도 포맷으로 입력')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    author = models.CharField(max_length=20)
+    messages = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+    def __str__(self):
+        return self.name
